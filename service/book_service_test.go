9dcac75789e6ebf636a9c9fcf4414a802deb4673
punchnnp <punch.nunnapat@gmail.com>
2023-06-16 16:19:02 +0700

    test table

--- service/book_service_test.go
+++ service/book_service_test.go
@@ -14,14 +14,27 @@ func TestGetAll(t *testing.T) {
 		Description string `db:"book_desc"`
 	}
 
+	tests := []struct {
+		name     string
+		expected []BookResponse
+	}{
+		{
+			name: "get all book",
+			expected: []BookResponse{
+				{BookID: 1, Name: "First Book", Description: "Tell something about this book"},
+				{BookID: 2, Name: "Second Book", Description: "This book is about how to cook"},
+			},
+		},
+	}
+
 	bookRepo := repository.NewBookRepositoryMock()
 	bookService := NewBookService(bookRepo)
 
-	books, _ := bookService.GetAllBooks()
-	expected := []BookResponse{
-		{BookID: 1, Name: "First Book", Description: "Tell something about this book"},
-		{BookID: 2, Name: "Second Book", Description: "This book is about how to cook"},
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			books, _ := bookService.GetAllBooks()
+			assert.EqualValues(t, tt.expected, books)
+		})
 	}
 
-	assert.Equal(t, expected, books)
 }
