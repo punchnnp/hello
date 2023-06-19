63b488fea8964a86106883b00be94c793e1b55df
punchnnp <punch.nunnapat@gmail.com>
2023-06-19 13:47:04 +0700

    create handler testing

---
+++ handler/book_test.go
@@ -0,0 +1,181 @@
+package handler_test
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+
+	"hello/handler"
+	"hello/repository"
+	"hello/service"
+)
+
+func TestGetAll(t *testing.T) {
+	tests := []struct {
+		description  string
+		route        string
+		expectedCode int
+	}{
+		{
+			description:  "GET HTTP status 200",
+			route:        "/api/book",
+			expectedCode: 200,
+		},
+		{
+			description:  "GET HTTP status 404, when route is not exists",
+			route:        "/api/something",
+			expectedCode: 404,
+		},
+	}
+
+	bookRepositoryMock := repository.NewBookRepositoryMock()
+	bookService := service.NewBookService(bookRepositoryMock)
+	bookHandler := handler.NewBookHandler(bookService)
+
+	app := fiber.New()
+	app.Get("/api/book", bookHandler.GetAllBooks)
+
+	for _, tt := range tests {
+		req := httptest.NewRequest("GET", tt.route, nil)
+		resp, _ := app.Test(req, 1)
+
+		assert.Equalf(t, tt.expectedCode, resp.StatusCode, tt.description)
+	}
+
+}
+
+func TestGetBookById(t *testing.T) {
+	tests := []struct {
+		description  string
+		route        string
+		expectedCode int
+	}{
+		{
+			description:  "GET HTTP status 200",
+			route:        "/api/book/1",
+			expectedCode: 200,
+		},
+		{
+			description:  "GET HTTP status 404, when route/bookID is not exist",
+			route:        "/api/book/1/book",
+			expectedCode: 404,
+		},
+	}
+
+	bookRepositoryMock := repository.NewBookRepositoryMock()
+	bookService := service.NewBookService(bookRepositoryMock)
+	bookHandler := handler.NewBookHandler(bookService)
+
+	app := fiber.New()
+	app.Get("/api/book/:id", bookHandler.GetBookById)
+
+	for _, tt := range tests {
+		req := httptest.NewRequest("GET", tt.route, nil)
+		resp, _ := app.Test(req, 1)
+
+		assert.Equalf(t, tt.expectedCode, resp.StatusCode, tt.description)
+	}
+
+}
+
+func TestAddNewBook(t *testing.T) {
+	tests := []struct {
+		description  string
+		route        string
+		expectedCode int
+	}{
+		{
+			description:  "GET HTTP 200 status",
+			route:        "/api/book",
+			expectedCode: 200,
+		},
+		{
+			description:  "GET HTTP 404 status when route is not exists",
+			route:        "/api/something",
+			expectedCode: 404,
+		},
+	}
+
+	bookRepositoryMock := repository.NewBookRepositoryMock()
+	bookService := service.NewBookService(bookRepositoryMock)
+	bookHandler := handler.NewBookHandler(bookService)
+
+	app := fiber.New()
+	app.Post("/api/book", bookHandler.AddNewBook)
+
+	for _, tt := range tests {
+		req := httptest.NewRequest("POST", tt.route, nil)
+		resp, _ := app.Test(req, 1)
+
+		assert.Equalf(t, tt.expectedCode, resp.StatusCode, tt.description)
+
+	}
+}
+
+func TestUpdateBook(t *testing.T) {
+	tests := []struct {
+		description  string
+		route        string
+		expectedCode int
+	}{
+		{
+			description:  "GET HTTP 200 status",
+			route:        "/api/book/2",
+			expectedCode: 200,
+		},
+		{
+			description:  "GET HTTP 404 status when route is not exists",
+			route:        "/api/book",
+			expectedCode: 404,
+		},
+	}
+
+	bookRepositoryMock := repository.NewBookRepositoryMock()
+	bookService := service.NewBookService(bookRepositoryMock)
+	bookHandler := handler.NewBookHandler(bookService)
+
+	app := fiber.New()
+	app.Post("/api/book/:id", bookHandler.AddNewBook)
+
+	for _, tt := range tests {
+		req := httptest.NewRequest("POST", tt.route, nil)
+		resp, _ := app.Test(req, 1)
+
+		assert.Equalf(t, tt.expectedCode, resp.StatusCode, tt.description)
+	}
+}
+
+func TestDeleteBook(t *testing.T) {
+	tests := []struct {
+		description  string
+		route        string
+		expectedCode int
+	}{
+		{
+			description:  "GET HTTP 200 status",
+			route:        "/api/book/2",
+			expectedCode: 200,
+		},
+		{
+			description:  "GET HTTP 404 status when route is not exists",
+			route:        "/api/book",
+			expectedCode: 404,
+		},
+	}
+
+	bookRepositoryMock := repository.NewBookRepositoryMock()
+	bookService := service.NewBookService(bookRepositoryMock)
+	bookHandler := handler.NewBookHandler(bookService)
+
+	app := fiber.New()
+	app.Delete("/api/book/:id", bookHandler.DeleteBook)
+
+	for _, tt := range tests {
+		req := httptest.NewRequest("DELETE", tt.route, nil)
+		resp, _ := app.Test(req, 1)
+
+		assert.Equalf(t, tt.expectedCode, resp.StatusCode, tt.description)
+	}
+}
